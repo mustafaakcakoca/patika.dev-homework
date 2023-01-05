b17f5c965cee42af1ce69692c1a41471e06241e0
mustafaakcakoca <116824365+mustafaakcakoca@users.noreply.github.com>
2023-01-05 18:42:12 +0300

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,97 @@
+package patikahomewrk;
+import java.util.Scanner;
+public class Calculator {
+    static int sum(int a , int b){
+        int result= a+b;
+        System.out.println("Toplam : "+result);
+        return result;
+    }
+    static int minus(int a , int b){
+        int result=a-b;
+        System.out.println("Çıkarma :" + result);
+        return result;
+
+    }
+    static int times(int a,int b){
+        int result=a*b;
+        System.out.println("Çarpma : "+times(a,b));
+        return result;
+    }
+    static int divided(int a , int b){
+        if(b==0){
+            System.out.println("İkinci Sayı 0'dan farklı olmalı");
+            return 0;
+        }
+        int result=a / b;
+        System.out.println("Bölüm : "+divided(a,b));
+        return result;
+    }
+    static int power(int a,int b){
+        int result = 1;
+        for(int i=1;i<=b;i++){
+        result *=a;
+        }
+        return result;
+    }
+    static int mod(int a, int b){
+        return a % b;
+    }
+    static void calc(int a , int b){
+        System.out.println("Çevresi : "+ (2 * (a+b)));
+        System.out.println("Alanı : "+ (a * b));
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int select;
+        String menu="1 - Toplama İşlemi \n"
+                +   "2 - Çıkarma İşlemi \n"
+                +   "3 - Çarpma  İşlemi \n"
+                +   "4 - Bölme   İşlemi \n"
+                +   "5 - Üslü Sayı Hesaplama\n"
+                +   "6 - Mod Alma \n"
+                +   "7 - Dikdörtgen Alan ve Çevre Hesabı"
+                +   "0 - Çıkış Yap ";
+
+        while(true){
+            System.out.println(menu);
+            System.out.print("Bir İşlem Seçiniz");
+            select=scan.nextInt();
+            System.out.print("İlk Sayıyı Girin : ");
+            int a= scan.nextInt();
+            System.out.print("İkinci Sayıyı Girin");
+            int b= scan.nextInt();
+
+            if(select==0)
+                break;
+
+            switch (select){
+                case 1:
+                    sum(a,b);
+                    break;
+                case 2:
+                    minus(a,b);
+                    break;
+                case 3:
+                    times(a,b);
+                    break;
+                case 4:
+                    divided(a,b);
+                    break;
+                case 5:
+                    System.out.println("Üs Hesaplama : "+power(a,b));
+                    break;
+                case 6:
+                    System.out.println("Mod İşlemi : "+mod(a,b));
+                    break;
+                case 7:
+                    calc(a,b);
+                default:
+                    System.out.println("Geçersiz Bir İşlem Yaptınız!");
+                    break;
+            }
+        }
+        System.out.println("Güle Güle");
+    }
+}
