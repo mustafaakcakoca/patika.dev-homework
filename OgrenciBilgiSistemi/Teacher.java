9993678b403ff492af193c8a6c2dc8bd0f70f425
mustafaakcakoca <116824365+mustafaakcakoca@users.noreply.github.com>
2023-01-18 16:55:49 +0300

    Add files via upload

---
+++ OgrenciBilgiSistemi/Teacher.java
@@ -0,0 +1,18 @@
+package OgrenciBilgiSistemi;
+
+public class Teacher {
+    String name;
+    String brach;
+    String mbPhNo;
+
+
+    Teacher(String name,String brach,String mbPhNo){
+        this.name=name;
+        this.brach=brach;
+        this.mbPhNo=mbPhNo;
+    }
+
+    void printTeacherInfo(){
+        System.out.println("Ad :   "+this.name+"\nBranş :   "+this.brach+"\nTel no :   "+this.mbPhNo);
+    }
+}
