b2992659734cd7af92e033cf31f41fb8ceb2056b
mustafaakcakoca <116824365+mustafaakcakoca@users.noreply.github.com>
2023-01-19 16:51:41 +0300

    Add files via upload

---
+++ MaasHesaplayici/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee employee=new Employee("Mustafa Akçakoca",2000,45,2021);
+
+        employee.toString();
+        System.out.println(employee.toString());
+
+    }
+}
